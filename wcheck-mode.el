f4a1ba28c3a4a2dd145dd5ae044515a539724cbe
Teemu Likonen <tlikonen@iki.fi>
2011-06-26 15:42:00 +0300

    Add link to the GPL3 license text in www.gnu.org

--- wcheck-mode.el
+++ wcheck-mode.el
@@ -19,8 +19,7 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs. If not, see <http://www.gnu.org/licenses/>.
+;; The license text: <http://www.gnu.org/licenses/gpl-3.0.html>
 
 
 ;; INSTALLATION
